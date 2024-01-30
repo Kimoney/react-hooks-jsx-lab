cb1fc9f3141fff9188a789577de436cf30d28561
John <johntaykimani@gmail.com>
2024-01-30 20:54:28 +0300

    Feat: Add JSX and inline styling on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const h1Style = {
+  color: "firebrick",
+  backgroundColor: "yellow",  
+};
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <h1 style={h1Style}>Your {name} is a Web Developer from Your {city} </h1>
+  </div>
+  );
 }
 
 export default Home;
